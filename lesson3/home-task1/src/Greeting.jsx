ac07b6e1da4d7b8f2cb64a2dfa335a5d01fdba88
Alexey <bumbumbu2@gmail.com>
2023-01-14 00:43:47 +0200

    1

--- lesson3/home-task1/src/Greeting.jsx
+++ lesson3/home-task1/src/Greeting.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import moment from 'moment';
 
-const Greeting = (props) => {
-  return (
-    <div className="greeting">{`My name is ${props.firstName} ${
-      props.lastName
-    }. I am ${moment().diff(props.birthDate, 'years')} years old`}</div>
-  );
-};
-
+const Greeting = (props) => (
+  <div className="greeting">{`My name is ${props.firstName} ${
+    props.lastName
+  }. I am ${moment().diff(props.birthDate, 'years')} years old`}</div>
+);
 export default Greeting;
