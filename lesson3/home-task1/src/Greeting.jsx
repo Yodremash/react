6c1c019983e9dcede9d238919065cc2d5db008d7
Alexey <bumbumbu2@gmail.com>
2023-01-14 01:56:45 +0200

    f

--- lesson3/home-task1/src/Greeting.jsx
+++ lesson3/home-task1/src/Greeting.jsx
@@ -4,7 +4,6 @@ import moment from 'moment';
 const Greeting = (props) => {
   const age = moment(new Date()).diff(moment(props.birthDate), 'years');
   return (
-    <div className="greeting">{`My name is ${props.firstName} ${props.lastName}. I am ${age} years old`}</div>
+    <div className="greeting">{`My name is ${props.firstName} ${props.lastName}. I'm ${age} years old`}</div>
   );
 };
-export default Greeting;
