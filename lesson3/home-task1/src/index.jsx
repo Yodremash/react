55b0e021a4d394458a701ce098c98f3142dfc51e
Alexey <bumbumbu2@gmail.com>
2023-01-14 01:51:00 +0200

    fix

--- lesson3/home-task1/src/index.jsx
+++ lesson3/home-task1/src/index.jsx
@@ -9,7 +9,7 @@ ReactDOM.render(
   <Greeting
     firstName="John"
     lastName="Doe"
-    birthDate={new Date('2006-01-01T11:11:11.819Z')}
+    birthDate={new Date('2006-01-01')}
   />,
   rootElement
 );
