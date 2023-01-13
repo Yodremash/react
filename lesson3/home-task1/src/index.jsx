d5c06a8466df1a8c02b5bf2546d8555c27316b92
Alexey <bumbumbu2@gmail.com>
2023-01-14 00:20:09 +0200

    fixed

--- lesson3/home-task1/src/index.jsx
+++ lesson3/home-task1/src/index.jsx
@@ -6,6 +6,6 @@ import Greeting from './Greeting';
 const rootElement = document.querySelector('#root');
 
 ReactDOM.render(
-  <Greeting firstName="John" lastName="Doe" birthDate="17" />,
+  <Greeting firstName={'John'} lastName={'Doe'} birthDate={17} />,
   rootElement
 );
