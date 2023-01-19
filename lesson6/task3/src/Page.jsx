0c56a8089e3c04bbd9a3c3a3e63f4c6232e83cb0
Alexey <bumbumbu2@gmail.com>
2023-01-19 14:33:56 +0200

    fix3

--- lesson6/task3/src/Page.jsx
+++ lesson6/task3/src/Page.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Info from './Info';
 
     const text1= 'Price is 500$. Available in 2 colors';
-    const text2= 'Price is 650$. Not available.'
+    const text2= 'Price is 650$. Not available in 1 color'
 
 
 class Page extends React.Component {
