039b78cf1c19f1c65168bc9164bfb46c0c9b544a
Alexey <bumbumbu2@gmail.com>
2023-01-19 15:07:26 +0200

    another final fix

--- lesson6/task3/src/Info.jsx
+++ lesson6/task3/src/Info.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 class Info extends React.Component {
     render = () =>
-      !this.props.info ? null : <div className="message">{this.props.info}</div>;
+      !this.props.info ? null : <div className="info">{this.props.info}</div>;
   }
 
   export default Info
