508fbb2b36f6e648c7a8936208b0b18646cde856
Alexey <bumbumbu2@gmail.com>
2023-02-21 12:57:26 +0200

    init files

---
+++ lesson11/task1/src/EvenNumbers.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class EvenNumbers extends React.Component {
+  shouldComponentUpdate(nextProps) {
+    return nextProps.number % 2 === 0
+  }
+
+  render() {
+    const { title, number } = this.props
+    return (
+      <div className="number">
+        <span className="number__title">{title}</span>
+        <span className="number__value">{number}</span>
+      </div>
+    )
+  }
+}
---
+++ lesson11/task1/src/OddNumbers.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class OddNumbers extends React.Component {
+  shouldComponentUpdate(nextProps) {
+    return nextProps.number % 2 === 1
+  }
+
+  render() {
+    const { title, number } = this.props
+    return (
+      <div className="number">
+        <span className="number__title">{title}</span>
+        <span className="number__value">{number}</span>
+      </div>
+    )
+  }
+}
+
+export default OddNumbers
